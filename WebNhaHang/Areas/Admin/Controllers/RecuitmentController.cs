8a4282e71fd78e8bdbb1681fe7939bf00bd6d810
XUOLO <xuanloc290901@gmail.com>
2023-05-07 23:55:21 +0700

    report excel docx img pdf

--- WebNhaHang/Areas/Admin/Controllers/RecuitmentController.cs
+++ WebNhaHang/Areas/Admin/Controllers/RecuitmentController.cs
@@ -1,4 +1,5 @@
-﻿using PagedList;
+﻿using Microsoft.Reporting.WebForms;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,5 +28,44 @@ namespace WebNhaHang.Areas.Admin.Controllers
             ViewBag.PageSize = pageSize;
             return View(items.ToPagedList(pageNumber, pageSize));
         }
+        public ActionResult Reports(string ReportType)
+        {
+            LocalReport localreport = new LocalReport();
+            localreport.ReportPath = Server.MapPath("~/report/ReportRecruitment.rdlc");
+
+            ReportDataSource reportDataSource = new ReportDataSource();
+            reportDataSource.Name = "DataSetRecruitment";
+            reportDataSource.Value = db.Aplications.ToList();
+            localreport.DataSources.Add(reportDataSource);
+            string reportType = ReportType;
+            string mimeType;
+            string encoding;
+            string fileNameExtension;
+            if (reportType == "Excel")
+            {
+                fileNameExtension = "xlsx";
+            }
+            if (reportType == "Word")
+            {
+                fileNameExtension = "docx";
+            }
+            if (reportType == "PDF")
+            {
+                fileNameExtension = "pdf";
+            }
+             else
+            {
+                fileNameExtension = "jpg";
+            }
+            string[] streams;
+            Warning[] warnings;
+            byte[] renderedByte;
+            renderedByte = localreport.Render(reportType,"",out mimeType, out encoding, out fileNameExtension,out streams,out warnings);
+            Response.AddHeader("content-disposition", "attachment;filename= Recruitment_report." + fileNameExtension);
+            return File(renderedByte, fileNameExtension);
+ 
+        }
+
+
     }
 }
